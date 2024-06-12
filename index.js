568daffae60240be7f4202866e2129a8ade24e85
lemuayala <lemuayala@gmail.com>
2024-06-12 13:41:59 -0300

    fix port for deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const userRoutes = require("./routes/userRoutes");
 require("./config/database");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
