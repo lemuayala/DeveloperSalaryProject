2511e142992fff4694762329d8800e540e291a00
lemuayala <lemuayala@gmail.com>
2024-06-13 20:31:55 -0300

    added swagger documentation (incomplete)

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+
 require("./config/database");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de Developer Salary",
+      version: "1.0.0",
+      description: "API para gestionar salarios de desarrolladores",
+    },
+  },
+  // Paths to files containing OpenAPI definitions
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +29,8 @@ app.use(bodyParser.json());
 
 // Usa las rutas definidas en userRoutes.js
 app.use("/api", userRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,116 @@ const {
 } = require("../controllers/userController");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /usuarios:
+ *   post:
+ *     summary: Crear usuario
+ *     description: Crea un nuevo usuario.
+ *     requestBody:
+ *       description: Datos del nuevo usuario
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               apellido:
+ *                 type: string
+ *               cargo:
+ *                 type: string
+ *               salario:
+ *                 type: number
+ *             required:
+ *               - nombre
+ *               - apellido
+ *               - salario
+ *     responses:
+ *       201:
+ *         description: Usuario creado exitosamente
+ *       400:
+ *         description: Error en los datos proporcionados
+ */
 router.post("/usuarios", crearUsuario);
+
+/**
+ * @swagger
+ * /usuarios:
+ *   get:
+ *     summary: Obtener todos los usuarios
+ *     description: Retorna una lista de todos los usuarios.
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios recuperada exitosamente
+ *       404:
+ *         description: No se encontraron usuarios
+ */
 router.get("/usuarios", obtenerUsuarios);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   put:
+ *     summary: Actualizar usuario
+ *     description: Actualiza un usuario existente por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: ID del usuario a actualizar
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Datos actualizados del usuario
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               apellido:
+ *                 type: string
+ *               cargo:
+ *                 type: string
+ *               salario:
+ *                 type: number
+ *             required:
+ *               - nombre
+ *               - apellido
+ *               - salario
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado exitosamente
+ *       400:
+ *         description: Error en los datos proporcionados
+ *       404:
+ *         description: Usuario no encontrado
+ */
 router.put("/usuarios/:id", actualizarUsuario);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   delete:
+ *     summary: Eliminar usuario
+ *     description: Elimina un usuario existente por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: ID del usuario a eliminar
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado exitosamente
+ *       404:
+ *         description: Usuario no encontrado
+ */
 router.delete("/usuarios/:id", eliminarUsuario);
 
 module.exports = router;
